1aa7c8c7d466d06962c193e15442b4e776e68018
nattawut.ch <b.nattawut79@gmail.com>
2026-05-28 14:22:23 +0700

    UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="th" className={notoThai.variable}>
       <body className={notoThai.className}>
         {/* Global Tech Watermark */}
-        <div className="fixed inset-0 pointer-events-none z-[-1] overflow-hidden flex items-center justify-center select-none">
+        <div className="fixed inset-0 pointer-events-none z-[-1] overflow-hidden flex items-center justify-center select-none transform-gpu will-change-transform">
           <QrCode 
             size={800} 
             className="text-indigo-500/[0.2] rotate-[15deg] translate-x-1/4 translate-y-1/4" 
